bb6b9011c0cace3a117182191430c021058be775
wayne80361 <wayne80361@gmail.com>
2023-09-23 16:57:14 -0400

    add renderLogo function in shaapes.js and test to create an index.html file

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // link inquirer to index.js
 const inquirer = require("inquirer");
+const { generateLogo } = require("./lib/shapes");
 
 // init function
-function init() {}
+function init() {
+  generateLogo();
+}
 
 init();
