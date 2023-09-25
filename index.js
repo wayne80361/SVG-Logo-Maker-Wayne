b3273d342ec0d1a3846810f909796259582d80d5
wayne80361 <wayne80361@gmail.com>
2023-09-25 18:38:07 -0400

    change require in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // link inquirer to index.js
 const inquirer = require("inquirer");
-const Shapes = require("./lib/shapes.js");
+const { Shapes } = require("./lib/shapes.js");
 
 // init();
 const newShape = new Shapes();
