f0df3231446dd459ed5046a518e653afee97b94a
wayne80361 <wayne80361@gmail.com>
2023-09-25 18:22:05 -0400

    use old fashion to export class constructors, and clean some more unused codes

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 // link inquirer to index.js
 const inquirer = require("inquirer");
-// const { renderLogo } = require("./lib/shapes");
 const Shapes = require("./lib/shapes.js");
-
-// init function
-// function init() {
-//   renderLogo();
-// }
 
 // init();
 const newShape = new Shapes();
