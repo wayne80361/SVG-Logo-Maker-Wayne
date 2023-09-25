df35081f5650e0a66ea03f164e92dd3c8943795c
wayne80361 <wayne80361@gmail.com>
2023-09-24 21:48:13 -0400

    add renderLogo function to Circle, Triangle and Square Class, change Shapes constructor property order

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // link inquirer to index.js
 const inquirer = require("inquirer");
-const { renderLogo } = require("./lib/shapes");
+// const { renderLogo } = require("./lib/shapes");
+const Circle = require("./lib/shapes.js");
+const Shapes = require("./lib/shapes.js");
+const Triangle = require("./lib/shapes.js");
+const Square = require("./lib/shapes.js");
 
 // init function
-function init() {
-  renderLogo();
-}
+// function init() {
+//   renderLogo();
+// }
 
-init();
+// init();
+const newCircle = new Circle("ABC", "red", "purple");
+newCircle.renderLogo();
